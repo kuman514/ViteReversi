4705f96c0034e374e70f0d338602fae82abcb5e8
kumankoishi.py <7iggif@naver.com>
2023-02-02 21:54:14 +0900

    test(modal-wrapper): add test

---
+++ src/components/atoms/ModalWrapper/index.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  beforeEach, describe, expect, test,
+} from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ModalWrapper from '.';
+
+describe('ModalWrapper', () => {
+  beforeEach(() => {
+    render(
+      <ModalWrapper>
+        Wrapper content
+      </ModalWrapper>,
+    );
+  });
+
+  test('should show the modal all the time', () => {
+    expect(screen.getByText(/Wrapper content/i)).toBeTruthy();
+  });
+});
