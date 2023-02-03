c37553fd1713f1256ba460954c0e616f843c0513
kumankoishi.py <7iggif@naver.com>
2023-02-03 22:55:48 +0900

    test(board-button): add clickability test

--- src/components/atoms/BoardButton/index.test.tsx
+++ src/components/atoms/BoardButton/index.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {
-  beforeEach, describe, expect, it,
+  beforeEach,
+  describe, expect, it,
 } from 'vitest';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import BoardButton from '.';
 import { BORDER_MIN, BORDER_MAX } from '^/constants';
 import { useGameStore } from '^/store';
+import { Who } from '^/types';
 
 describe('BoardButton', () => {
   beforeEach(() => {
@@ -33,5 +35,31 @@ describe('BoardButton', () => {
         <BoardButton row={BORDER_MAX + 1} col={BORDER_MAX + 1} />,
       );
     }).toThrowError();
+  });
+
+  it('is clickable when clickable', () => {
+    expect(useGameStore.getState().currentTurn).equals(Who.PLAYER_1);
+    const { container } = render(
+      <BoardButton row={2} col={4} />,
+    );
+    const button = container.querySelector('button');
+    if (!button) {
+      throw Error('The container returened nullish');
+    }
+    fireEvent.click(button);
+    expect(useGameStore.getState().currentTurn).equals(Who.PLAYER_2);
+  });
+
+  it('is not clickable when unclickable', () => {
+    expect(useGameStore.getState().currentTurn).equals(Who.PLAYER_1);
+    const { container } = render(
+      <BoardButton row={0} col={0} />,
+    );
+    const button = container.querySelector('button');
+    if (!button) {
+      throw Error('The container returened nullish');
+    }
+    fireEvent.click(button);
+    expect(useGameStore.getState().currentTurn).equals(Who.PLAYER_1);
   });
 });
