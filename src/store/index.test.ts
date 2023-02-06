9728c4d73928ba57fc2a1126aeeef326166c35fe
kumankoishi.py <7iggif@naver.com>
2023-02-07 05:05:59 +0900

    test(board-stote): test convert to is-available and init game state

--- src/store/index.test.ts
+++ src/store/index.test.ts
@@ -34,7 +34,34 @@ const convertStringToBoard: (boardStr: string) => BoardState | undefined = (boar
   return result;
 };
 
-describe('convertStringToBoard', () => {
+const convertStringToIsAvailable: (boardStr: string) => boolean[][] | undefined = (boardStr) => {
+  const board = boardStr.trim().split('\n').map((row) => row.trim().split(''));
+
+  if (board.length !== LENGTH || board.some((row) => row.length !== LENGTH)) {
+    return undefined;
+  }
+
+  const result: boolean[][] = [];
+  for (let i = BORDER_MIN; i <= BORDER_MAX; i++) {
+    result.push([]);
+    for (let j = BORDER_MIN; j <= BORDER_MAX; j++) {
+      result[i].push((() => {
+        switch (board[i][j]) {
+          case '0':
+            return false;
+          case '1':
+            return true;
+          default:
+            throw Error('Who should be just one of EMPTY, PLAYER_1, or PLAYER_2.');
+        }
+      })());
+    }
+  }
+
+  return result;
+};
+
+describe('convertStringToBoard and convertStringToIsAvailable', () => {
   it('should not throw in appropriate length', async () => {
     expect(convertStringToBoard(`
       00000000
@@ -90,5 +117,104 @@ describe('convertStringToBoard', () => {
       00000000
       00000000
     `)).toBeFalsy();
+
+    expect(convertStringToIsAvailable(`
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+    `)).toBeTruthy();
+
+    expect(convertStringToIsAvailable(`
+      00000000
+      00000000
+      000000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+    `)).toBeFalsy();
+
+    expect(convertStringToIsAvailable(`
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      0000000
+    `)).toBeFalsy();
+
+    expect(convertStringToIsAvailable(`
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+    `)).toBeFalsy();
+
+    expect(convertStringToIsAvailable(`
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+      00000000
+    `)).toBeFalsy();
+  });
+});
+
+describe('Board Store State', () => {
+  beforeEach(() => {
+    useGameStore.getState().reset();
+  });
+
+  it('initial state', async () => {
+    expect(useGameStore.getState().boardState).toStrictEqual(convertStringToBoard(`
+      00000000
+      00000000
+      00000000
+      00012000
+      00021000
+      00000000
+      00000000
+      00000000
+    `));
+  });
+
+  it('game', async () => {
+    // Initial state
+    expect(useGameStore.getState().boardState).toStrictEqual(convertStringToBoard(`
+      00000000
+      00000000
+      00000000
+      00012000
+      00021000
+      00000000
+      00000000
+      00000000
+    `));
+    expect(useGameStore.getState().currentTurn).toEqual(Who.PLAYER_1);
+    expect(useGameStore.getState().isAvailable).toEqual(convertStringToIsAvailable(`
+      00000000
+      00000000
+      00001000
+      00000100
+      00100000
+      00010000
+      00000000
+      00000000
+    `));
   });
 });
